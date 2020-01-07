66a0959497607157844abf2979e4380e5d032e0f
Connor Filipovic <cfilipovic@local.niirs10.com>
2020-01-07 16:49:44 -0600

    Add projects in grid

--- src/app/portfolio/portfolio.service.ts
+++ src/app/portfolio/portfolio.service.ts
@@ -8,10 +8,10 @@ export class PortfolioService {
   constructor() { }
 
   initProjects() {
-    const blog = {title: "title", description: "description", languages: ["Java"], image: "./assets/portfolio/blog.png", version: "1.0", url: "/blog"};
-    const bugtracker = {title: "Bug Tracker", description: "description", languages: ["Java"], image: "./assets/portfolio/bugtracker.png", version: "0.5", url: "/bugtracker"};
-    const design = {title: "Daily UI Design", description: "Daily UI challenge to improve my design skills", languages: ["Angular 8.0", "NodeJS", "CSS", "HTML"], image: "./assets/portfolio/design.png", version: "1.0", url: "/design"};
-    const news = {title: "title", description: "description", languages: ["Java"], image: "./assets/portfolio/news.png", version: "1.0", url: "/news"};
+    const blog = {title: "Blog", description: "Simple blog site so I can post general tips or tutorials", date: "12/13/2019", languages: ["Angular", "Asychronous", "Service"], image: "./assets/portfolio/news.png", version: "1.0", url: "/blog"};
+    const bugtracker = {title: "Bug Tracker", description: "Simple bug tracker for developers to create and file requests and update their progress/resolve bugs as work has been done.", date: "12/20/2019",languages: ["Java"], image: "./assets/portfolio/news.png", version: "0.5", url: "/bugtracker"};
+    const design = {title: "Daily UI Design", description: "Daily UI challenge to improve my design skills.  Focusing on mobile application design using Adobe XD", date: this.getTodaysDate(),languages: ["Adobe XD", "Web Design", ], image: "./assets/portfolio/news.png", version: "1.0", url: "/design"};
+    const news = {title: "News", description: "Simple news aggregator site.", date: "12/31/2019", languages: ["Angular", "Asychronous", "Service"], image: "./assets/portfolio/news.png", version: "1.0", url: "/news"};
 
     const myObservable = of([
       blog,
@@ -22,5 +22,16 @@ export class PortfolioService {
 
 
     return myObservable;
+  }
+
+  getTodaysDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    var todayStr = mm + '/' + dd + '/' + yyyy;
+
+    return todayStr;
   }
 }
